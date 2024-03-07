154e00f49c66f624cda237fed655f74b821f7b3e
Dana Marble <dana.marble@gmail.com>
2024-03-07 04:24:45 +0000

    change flags

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -19,7 +19,7 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-#$(INSTALL) -d 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/writer
+#
 #$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 #
 
@@ -31,6 +31,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
  $(INSTALL) -m 0755 $(@D)/finder-app/conf/* $(TARGET_DIR)/etc/finder-app/conf/
  $(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/finder.sh
  $(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
+ $(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/writer
 
 endef
 
